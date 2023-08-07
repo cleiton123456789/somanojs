22f2a1b67e0f487144158315cd68b094813f7fdb
Samuel Luigi <139802358+cleiton123456789@users.noreply.github.com>
2023-08-07 16:10:30 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,10 @@
+    function Soma(a,b) {
+        if (a + b === 50) {
+          console.log(a + b === 50 + "e igual a 50.");
+        }else {
+            console.log(a + b + " nao e igual a 50 .");
+
+        }
+    }
+    console.log(Soma(30,10))
+       
